aba6a508b6089ab6b756389a00290698facfd034
Tim <tipfom@hotmail.de>
2018-04-07 15:22:21 +0200

    changed gold and damage label alignment

---
+++ Universal/UI/Elements/Aligner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Universal.UI.Layout;
+
+namespace Universal.UI.Elements
+{
+    public class Aligner : Element {
+        public Aligner (Screen owner, Container container) : base(owner, container, 0, false) {
+        }
+
+        public override bool HandleTouch (Touch.Action action, Touch touch) {
+            return false;
+        }
+
+        public override IEnumerable<RenderableElement> Draw ( ) {
+            yield break;
+        }
+    }
+}
--- Universal/UI/Screens/GameplayScreen.cs
+++ Universal/UI/Screens/GameplayScreen.cs
@@ -24,6 +24,8 @@ namespace Universal.UI.Screens {
         private ProgressBar stageProgressBar;
         private ProgressBar healthLeftBar;
         private Countdown countdown;
+        private Aligner goldLabelAligner;
+        private Aligner damageLabelAligner;
         private List<FadeTextAnimation> textAnimations = new List<FadeTextAnimation>( );
 
         private float _multiplier = 1f;
@@ -42,8 +44,11 @@ namespace Universal.UI.Screens {
             multiplierBar = new ProgressBar(this, new Container(new Margin(0, 1f, 0, 0.0125f), MarginType.Relative), Depth.Foreground) { Max = 10, Value = multiplier, Color = Color.Gold };
             healthLeftBar = new ProgressBar(this, new Container(new Margin(0f, 1f, 0.3f, 0.05f), MarginType.Relative), Depth.Foreground) { Max = 1, Value = 1, Color = new Color(255, 20, 20, 255) };
 
+            goldLabelAligner = new Aligner(this, new Container(new Margin(0.1f, 0f, 0f, 0f), MarginType.Absolute, Position.Left | Position.Top, Position.Left | Position.Top));
+            damageLabelAligner = new Aligner(this, new Container(new Margin(0, 0.1f, 0f, 0f), MarginType.Absolute, Position.Right | Position.Top, Position.Right | Position.Top));
+
             CoinLabel goldLabel = new CoinLabel(this, new Container(new Margin(0, 0.025f), MarginType.Absolute, anchor: Position.Center | Position.Top, dock: Position.Center | Position.Bottom, relative: multiplierBar), 0.05f, Depth.Foreground, Label.TextAlignment.Left);
-            
+
             Label stageLabel = new Label(this, new Container(new Margin(0.025f, 0.025f), MarginType.Absolute, anchor: Position.Top | Position.Right, dock: Position.Right | Position.Bottom, relative: multiplierBar), 0.09f, Manager.State.Stage.ToString( ), new Color(100, 100, 100, 100), Depth.Foreground, Label.TextAlignment.Right);
             Manager.State.StageChanged += (newStage) => {
                 stageLabel.Text = newStage.ToString( );
@@ -52,11 +57,11 @@ namespace Universal.UI.Screens {
             targetArea = new TargetArea(this, new Container(new Margin(0f, 1f, 0.35f, 0.625f), MarginType.Relative, Position.Left | Position.Top), Depth.Center, ChallengeProgressCallback, GetTapChallenge);
 
             countdown = new Countdown(this, new Container(new Margin(0f, 1f, 0.35f, 0.625f), MarginType.Relative), Depth.Foreground, 0.2f, 3);
-            
+
             countdown.Finished += ( ) => {
                 Start( );
             };
-            
+
             EntityRenderer.VertexSize = map.Container.Height / 3f;
 
             Prepare( );
@@ -119,7 +124,7 @@ namespace Universal.UI.Screens {
             if (isHit) {
                 player.Attack( );
                 float damage = player.GetDamage(type);
-                textAnimations.Add(new FadeTextAnimation(new Label(this, new Container(new Margin(0, 0, map.MobPosition.Y - 0.075f, 0), MarginType.Absolute, anchor: Position.Center | Position.Top, dock: Position.Center | Position.Top), 0.05f, damage.ToString( ), alignment: Label.TextAlignment.Center), 0.05f, 0.75f));
+                textAnimations.Add(new FadeTextAnimation(new Label(this, new Container(new Margin(0, 0, map.MobPosition.Y - 0.075f, 0), MarginType.Absolute, anchor: Position.Center | Position.Top, dock: Position.Right | Position.Top, relative: damageLabelAligner), 0.05f, damage.ToString( ), alignment: Label.TextAlignment.Center), 0.05f, 0.75f));
                 if ((mobs[0].Health -= damage) < 0) {
                     mobs[0].Die(null);
 
@@ -139,7 +144,7 @@ namespace Universal.UI.Screens {
 
         private void Next ( ) {
             int gold = (int)(mobs[0].Value * multiplier);
-            textAnimations.Add(new FadeTextAnimation(new Label(this, new Container(new Margin(0, 0, map.MobPosition.Y - 0.075f, 0), MarginType.Absolute, anchor: Position.Center | Position.Top, dock: Position.Center | Position.Top), 0.075f, "+" + gold.ToString( ), Color.Gold, Depth.Foreground, Label.TextAlignment.Center), 0.075f, 0.5f));
+            textAnimations.Add(new FadeTextAnimation(new Label(this, new Container(new Margin(0, 0, map.MobPosition.Y - 0.075f, 0), MarginType.Absolute, anchor: Position.Center | Position.Top, dock: Position.Left | Position.Top, relative: goldLabelAligner), 0.075f, "+" + gold.ToString( ), Color.Gold, Depth.Foreground, Label.TextAlignment.Center), 0.075f, 0.5f));
             Manager.State.Gold += gold;
 
             mobs.Insert(0, GetMob( ));
